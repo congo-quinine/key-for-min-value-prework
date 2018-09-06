d82da2248e8714c0f07e68aa1acda391a2ab1b3c
Rob Parker <into.the.parker@gmail.com>
2018-09-06 09:46:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  
+  if name_hash.size < 1
+    return nil
+
     holder = 99999
     key_return = ''
     name_hash.each do |key, value|
